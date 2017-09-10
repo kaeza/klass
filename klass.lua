fe161eb9e5e9a1c96d234f6b2204e4e3cb22738a
Diego Martínez <kaeza@users.noreply.github.com>
2017-09-09 22:24:42 -0300

    Add documentation.

--- klass.lua
+++ klass.lua
@@ -1,17 +1,42 @@
 
 ---
+-- Minimalistic class system for Lua.
+--
 -- @classmod klass
+-- @author Diego Martínez <https://github.com/kaeza>
+-- @license MIT. See `LICENSE.md` for details.
 
 local setmetatable, rawequal, pairs, tostring =
       setmetatable, rawequal, pairs, tostring
 
 local klass = { }
 klass.__index = klass
+
+---
+-- Name of the class.
+--
+-- @tfield string __name
 klass.__name = "klass"
 
 ---
--- @tparam string name
--- @treturn table
+-- Superclass of the instance or class.
+--
+-- @tfield klass __super
+klass.__super = nil
+
+---
+-- Extend a class.
+--
+-- @tparam string name Name of the new class.
+-- @treturn table New class table.
+-- @usage
+--  local sub = klass:extend("sub")
+--  function sub:init(x, y)
+--      -- constructor
+--  end
+--  function sub:dosomething()
+--      -- do something here
+--  end
 function klass:extend(name)
 	local c = setmetatable({ }, self)
 	for k, v in pairs(self) do
@@ -24,8 +49,17 @@ function klass:extend(name)
 end
 
 ---
--- @tparam any ...
--- @treturn table
+-- Construct a new instance.
+--
+-- @tparam any ... Arguments to be passed to `init`.
+-- @treturn table New instance.
+-- @usage
+--  local sub = klass:extend("sub")
+--  function sub:init(x)
+--      self.x = x
+--  end
+--  local inst = sub("foobar")
+--  print(inst.x) --> foobar
 function klass:__call(...)
 	local inst = setmetatable({ }, self)
 	inst:init(...)
@@ -33,8 +67,17 @@ function klass:__call(...)
 end
 
 ---
--- @tparam table c
--- @treturn boolean
+-- Test if this object is an instance of a class.
+-- @tparam table c Class.
+-- @treturn boolean True if it is an instance, false otherwise.
+-- @usage
+--  local sub = klass:extend("sub")
+--  local klsinst = klass()
+--  local subinst = sub()
+--  print(subinst:isinstance(klass)) --> true
+--  print(subinst:isinstance(sub))   --> true
+--  print(klsinst:isinstance(klass)) --> true
+--  print(klsinst:isinstance(sub))   --> false
 function klass:isinstance(c)
 	local mycls = self.__index
 	while mycls do
@@ -47,14 +90,24 @@ function klass:isinstance(c)
 end
 
 ---
--- @param ...
+-- Instance initializer.
+--
+-- The default implementation does nothing.
+-- It should be overridden in subclasses to do something useful.
+--
+-- @param ... Arguments passed to `__call`.
 function klass:init(...)
 end
 
 ---
--- @treturn string
+-- Get a string representation of this instance.
+--
+-- The default implementation returns "<NAME>".
+-- It should be overridden in subclasses if more information is desired.
+--
+-- @treturn string String representation.
 function klass:__tostring()
 	return "<"..tostring(self.__name)..">"
 end
 
-return setmetatable(klass, { __call=klass.extend })
+return setmetatable(klass, { __call=klass.__call })
